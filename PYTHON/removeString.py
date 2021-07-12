3174a04ed6af39acb5e72f4da08b4c9e02707724
sourabhv7 <sourabhverm4@gmail.com>
2021-07-12 15:39:16 +0530

    July 09

---
+++ PYTHON/removeString.py
@@ -0,0 +1,11 @@
+''' Code By Sourabh Verma
+    WAP program to remove alphabets from a string.
+    Input - daily buggers 123
+    Output- 123
+'''
+mystr = ' Sourabh Verma 777'
+newstr = ''
+for item in range(0,len(mystr)):
+    if mystr[item] !=' ' and (mystr[item] < 'a' or mystr[item] >'z') and (mystr[item] < 'A' or mystr[item] >'Z'):
+        newstr += mystr[item]
+print(newstr)
