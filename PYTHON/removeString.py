2a21eb32440e161a88ecc04762cc65a6f26151c0
sourabhv7 <sourabhverm4@gmail.com>
2021-07-12 15:49:12 +0530

    July 09

--- PYTHON/removeString.py
+++ PYTHON/removeString.py
@@ -3,7 +3,8 @@
     Input - daily buggers 123
     Output- 123
 '''
-mystr = ' Sourabh Verma 777'
+mystr = 'Sourabh Verma 777'
+print(mystr)
 newstr = ''
 for item in range(0,len(mystr)):
     if mystr[item] !=' ' and (mystr[item] < 'a' or mystr[item] >'z') and (mystr[item] < 'A' or mystr[item] >'Z'):
